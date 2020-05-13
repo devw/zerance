d761c0a207844b44fa6bfbdfcacb6fc34998c6d3
apierro <antonio.pierro@gmail.com>
2020-05-13 19:08:21 +0200

    adding slide caption

--- src/components/slider/slide-1.js
+++ src/components/slider/slide-1.js
@@ -6,7 +6,6 @@ import slide1 from "../../images/artboard_1.jpg";
 export const Slide1 = () => (
     <div id="slide-1" className={classes.slide}>
         <img src={slide1} alt="" />
-        sadassd
         <SlideCaption></SlideCaption>
         <a href="#slide-2">
             <i style={{ transform: "rotate(135deg)" }}></i>
--- src/components/slider/slide-caption.js
+++ src/components/slider/slide-caption.js
@@ -3,6 +3,12 @@ import classes from "./slide-caption.module.css";
 
 export const SlideCaption = () => (
     <span className={classes.caption}>
-        asddadaslkj asdkask asdaskj asldkajldas aslkjdalkajs
+        <h2>From scratch /</h2>
+        <h1>Seasonly</h1>
+        <div>
+            Développement site from scratch et d’un quiz personnalisé avec
+            abonnement. Développement site from scratch et d’un quiz
+            personnalisé avec abonnement.
+        </div>
     </span>
 );
