221f3db6b2893941856b24b9cb582ff377df4405
apierro <antonio.pierro@gmail.com>
2020-05-13 18:02:20 +0200

    adding slide-caption component

--- src/components/slider/slide-1.js
+++ src/components/slider/slide-1.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./slide.module.css";
+import { SlideCaption } from "./slide-caption";
 import slide1 from "../../images/artboard_1.jpg";
 
 export const Slide1 = () => (
     <div id="slide-1" className={classes.slide}>
         <img src={slide1} alt="" />
-        <span>asdadsa</span>
+        sadassd
+        <SlideCaption></SlideCaption>
         <a href="#slide-2">
             <i style={{ transform: "rotate(135deg)" }}></i>
         </a>
---
+++ src/components/slider/slide-caption.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./slide-caption.module.css";
+
+export const SlideCaption = () => (
+    <span className={classes.caption}>
+        asddadaslkj asdkask asdaskj asldkajldas aslkjdalkajs
+    </span>
+);
