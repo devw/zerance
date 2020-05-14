8057fafd0004597ae13d00e3ea3a3e914e676924
apierro <antonio.pierro@gmail.com>
2020-05-14 23:10:39 +0200

    adding mediaquery to portfolio component

---
+++ src/components/portfolio/item.js
@@ -0,0 +1,8 @@
+import React from "react";
+import img from "../../images/calque_4.png";
+
+export const Item = () => (
+    <a href="https://yokoshop.com">
+        <img src={img} alt="" />
+    </a>
+);
