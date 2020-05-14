78779b54a98a300ad01900cc79cf8aaca5a4ad98
apierro <antonio.pierro@gmail.com>
2020-05-14 13:20:45 +0200

    adding TextInfinite component

--- src/components/text-infinite.js
+++ src/components/text-infinite.js
@@ -1,10 +1,19 @@
 import React from "react";
+import classes from "./text-infinite.module.css";
 
 export const TextInfinite = () => (
-    <div class="zerance text-infinite">
-        <div class="text-animation">
-            Qu’est ce qui est jaune et qui attend ? / Quand c’est trop, c’est
-            Tropico / Frédéric Diefental / Good cat Bad cat /
-        </div>
-    </div>
+    <section className={classes["infinite-text"]}>
+        <p>
+            <span>
+                Qu’est ce qui est jaune et qui attend ? / Quand c’est trop,
+                c’est Tropico / Frédéric Diefental / Good cat Bad cat /
+            </span>
+        </p>
+        <p>
+            <span>
+                Qu’est ce qui est jaune et qui attend ? / Quand c’est trop,
+                c’est Tropico / Frédéric Diefental / Good cat Bad cat /
+            </span>
+        </p>
+    </section>
 );
