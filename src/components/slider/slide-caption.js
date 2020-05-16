1ec1c56eff4aba4f5e5fce37cb8b0aee922ccd74
apierro <antonio.pierro@gmail.com>
2020-05-16 09:42:15 +0200

    adding brush_stroke to slider caption

--- src/components/slider/slide-caption.js
+++ src/components/slider/slide-caption.js
@@ -4,11 +4,12 @@ import classes from "./slide-caption.module.css";
 export const SlideCaption = () => (
     <span className={classes.caption}>
         <h2>From scratch /</h2>
-        <h1>Seasonly</h1>
+        <h1>seasonly</h1>
         <div>
             Développement site from scratch et d’un quiz personnalisé avec
             abonnement. Développement site from scratch et d’un quiz
             personnalisé avec abonnement.
         </div>
+        <h4>&lt; Voir plus &#47;&gt;</h4>
     </span>
 );
