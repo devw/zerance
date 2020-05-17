35b16abe40efbc60372a3d805fb664c0c99a500b
apierro <antonio.pierro@gmail.com>
2020-05-17 11:11:02 +0200

    changing banner text direction for small screen

--- src/components/top/banner.js
+++ src/components/top/banner.js
@@ -3,11 +3,14 @@ import classes from "./banner.module.css";
 
 export const Banner = () => (
     <div className={classes.banner}>
+        <h3>Since 2016</h3>
+        <h2>
+            &lt; We create from scratch amazing <span>Shopify</span>
+            &#47;&gt;
+        </h2>
         <div>
-            <h3>Since 2016</h3>
             <h2>
-                &lt; We create from scratch amazing <span>Shopify</span>{" "}
-                &#47;&gt;
+                <span>Shopify &#47;&gt;</span>
             </h2>
         </div>
     </div>
