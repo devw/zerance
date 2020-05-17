b29b9418b4ed0daf6b6f194e4dd722ccfca67d8c
apierro <antonio.pierro@gmail.com>
2020-05-17 18:05:53 +0200

    refactoring banner css

--- src/components/navbar-and-banner/banner.js
+++ src/components/navbar-and-banner/banner.js
@@ -5,13 +5,10 @@ export const Banner = () => (
     <div className={classes.banner}>
         <h3>Since 2016</h3>
         <h2>
-            &lt; We create from scratch amazing <span>Shopify</span>
-            &#47;&gt;
+            &lt; We create from scratch amazing <span>Shopify</span> &#47;&gt;
         </h2>
-        <div>
-            <h2>
-                <span>Shopify &#47;&gt;</span>
-            </h2>
-        </div>
+        <h2>
+            <span>Shopify &#47;&gt;</span>
+        </h2>
     </div>
 );
