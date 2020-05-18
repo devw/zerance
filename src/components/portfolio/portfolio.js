d523dcb1956b2829ffa057237007bc90df7d5d07
apierro <antonio.pierro@gmail.com>
2020-05-18 08:42:08 +0200

    improving the navbar

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -3,7 +3,7 @@ import { Item } from "./item";
 import classes from "./portfolio.module.css";
 
 export const Portfolio = () => (
-    <div id="clients" style={{ "background-color": "#000", padding: "5vw" }}>
+    <div id="clients" style={{ padding: "5vw" }}>
         <div className={classes.portfolio}>
             <h2>Nos client sont Geniaux !</h2>
             {Array.from(Array(20)).map((x, i) => (
